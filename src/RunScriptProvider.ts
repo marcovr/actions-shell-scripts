07a6e7432b1b16fd75276f9fb22f143120f0f5ae
Marco von Raumer <marco.von.raumer@digitecgalaxus.ch>
2025-07-22 12:20:40 +0200

    Run script in terminal instead of webview

--- src/RunScriptProvider.ts
+++ src/RunScriptProvider.ts
@@ -1,8 +1,6 @@
-import AnsiToHtml from "ansi-to-html";
-import { spawn } from "child_process";
+import os from "os";
 import fs from "fs";
 import path from "path";
-// import terminate from "terminate";
 import {
   CancellationToken,
   CodeLens,
@@ -13,8 +11,6 @@ import {
   Position,
   Range,
   TextDocument,
-  ViewColumn,
-  WebviewPanel,
   window,
   workspace,
 } from "vscode";
@@ -39,7 +35,7 @@ export class RunScriptProviderImpl implements CodeLensProvider {
     );
     const codeLensRange = new Range(codeLensPos, codeLensPos);
     const codeLens = new CodeLens(codeLensRange, {
-      title: "▶️ Run YAML with Script",
+      title: "▶️ Run Script",
       command: "yaml-with-script.run",
       arguments: [script],
     });
@@ -75,99 +71,15 @@ export class RunScriptProviderImpl implements CodeLensProvider {
 
 export const RunScriptProvider = RunScriptProviderImpl;
 
-let panel: WebviewPanel | undefined;
-let scriptRunning = false;
 commands.registerCommand("yaml-with-script.run", (script: Script) => {
-  if (scriptRunning) {
-    window.showWarningMessage("A script is already running!");
-    return;
-  }
+  const tmpFilePath = path.join(os.tmpdir(), "_shellcheck_script.sh");
+  fs.writeFileSync(tmpFilePath, script.getContent(), "utf8");
 
-  scriptRunning = true;
+  const terminalName = "GH-actions-script";
 
-  if (!panel) {
-    panel = window.createWebviewPanel(
-      "yamlWithScriptOutput",
-      "YAML with Script - Output",
-      ViewColumn.Beside,
-      { enableScripts: true, retainContextWhenHidden: true }
-    );
-  } else {
-    panel.webview.html = "";
-  }
-
-  const htmlPath = path.join(script.extensionPath, "html", "console.html");
-  panel.webview.html = fs
-    .readFileSync(htmlPath, "utf8")
-    .replace(/>\s+</g, "><");
-
-  const ansiToHtml = new AnsiToHtml({
-    fg: "#cccccc",
-    bg: "#1e1e1e",
-    colors: {
-      0: "#000000",
-      1: "#cd3131",
-      2: "#0dbc79",
-      3: "#e5e510",
-      4: "#2472c8",
-      5: "#bc3fbc",
-      6: "#11a8cd",
-      7: "#e5e5e5",
-      8: "#666666",
-      9: "#f14c4c",
-      10: "#23d18b",
-      11: "#f5f543",
-      12: "#3b8eea",
-      13: "#d670d6",
-      14: "#29b8db",
-      15: "#e5e5e5",
-    },
-  });
-
-  const config = workspace.getConfiguration("yaml-with-script");
-  const baseScript = config.get("baseScript", "");
-
-  const runScriptCommand = baseScript
-    ? `source ${baseScript} && ${script.getContent()}`
-    : script.getContent();
-
-  const cwd = workspace.workspaceFolders?.[0]?.uri.fsPath;
-  const process = spawn(runScriptCommand, [], { shell: true, cwd });
-
-  panel.onDidDispose(() => {
-    if (process.pid) {
-      // terminate(process.pid);
-    }
-    panel = undefined;
-  });
-
-  panel.webview.onDidReceiveMessage((message) => {
-    if (message.command === "stopScript" && process.pid) {
-      // terminate(process.pid);
-    }
-  });
-
-  const handleProcessOutput = (data: Buffer) => {
-    if (panel) {
-      panel.webview.postMessage({
-        type: "updateContent",
-        value: ansiToHtml.toHtml(data.toString("utf-8")),
-      });
-    }
-  };
-
-  process.stdout.on("data", handleProcessOutput);
-  process.stderr.on("data", handleProcessOutput);
-
-  process.on("exit", (code, signal) => {
-    const message = signal ? "Cancelled!" : `Finished (${code})`;
-    if (panel) {
-      panel.webview.postMessage({
-        type: "scriptStopped",
-        value: message,
-      });
-    }
-
-    scriptRunning = false;
-  });
+  const terminal =
+    window.terminals.find((t) => t.name === terminalName) ||
+    window.createTerminal(terminalName);
+  terminal.show();
+  terminal.sendText(` bash --noprofile --norc -e -o pipefail ${tmpFilePath}`);
 });
